0438c8e1e77213548c9ce3de19f48af73486110c
przem <przemyslawwlodarczyk90@gmail.om>
2023-06-21 18:54:17 +0200

    Day 02 SDA

---
+++ src/Homework/Homework.java
@@ -0,0 +1,17 @@
+package Homework;
+
+import java.util.Scanner;
+
+class RomanArab {
+
+    private static final String[] NUMERALS ={ "I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M" };
+    public static int romanToArab (String roman)  {//throws RomanArabException {
+
+            }
+    public static String arabToRoman (int arab) {//throws RomanArabException {
+    }
+
+    public static void main(String[] args) {
+
+        }}
+ 
